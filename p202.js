58e6e100185f18d34665afdf18c37bf911dfe039
lala97 <lalamamedova97@gmail.com>
2017-06-06 16:49:01 +0400

    finaly slider

--- p202.js
+++ p202.js
@@ -1,45 +1,50 @@
 $(document).ready(function() {
   $('.thumb img').click(function() {
-     var source=$(this).attr('src',);
-      console.log(source);
-      $('.slider').fadeTo('fast',0.4,function() {
-        $(this).css({
-          'background-image' : 'url('+source+')',
-          'transition' :'all .1s'
-        })
-      }).fadeTo('fast',1);
+    var source= $(this).attr('src');
+    $('.slider').fadeTo('slow',0.6,function() {
+      $(this).css({
+        'background-image' : 'url(' + source + ')'
+      })
+    }).fadeTo('slow',1)
 
   })
-var margin=0;
-var count=0;
-  $('#next').click(function() {
-    var width=$('.subMain').children().length;
-    var contentWidth=$('.subMain').children().width();
-    margin+=contentWidth;
-    count++;
-    // console.log(count);
-    if (count >= width) {
-      margin=0;
-      count=0;
-    }
-    $('.subMain').css({
-      'marginLeft' :-margin,
-      'transition' :"all 1s"
-    })
-  })
-  $('#previous').click(function(){
-    var width=$('.subMain').children().length;
-    var contentWidth=$('.subMain').children().width();
-    margin-=contentWidth;
-    count--;
-    // console.log(count);
-    if (count <= length) {
-      margin-=contentWidth ;
-      count=length-1;
-    }
-    $('.subMain').css({
-      'marginLeft' :margin,
-      'transition' :"all 1s"
-  })
 })
+// console.log();
+ childrenWidth=$('.slider').width();
+ length=$('.subMain').children().length;
+  prev = 0;
+ margin=0;
+
+ function next() {
+   prev++;
+    margin+=childrenWidth;
+    if (prev >=length) {
+        margin = 0;
+        prev=0;
+    }
+    $('.subMain').css({
+    'marginLeft' :-margin,
+    'transition' : 'all 1s'
+  })
+ }
+
+ function previous() {
+   prev--;
+    margin-=childrenWidth;
+    if (prev <= 0) {
+        margin = childrenWidth*(length-1);
+        prev=length;
+    }
+    $('.subMain').css({
+    'marginLeft' :-margin,
+    'transition' : 'all 1s'
+   })
+ }
+
+$('#next').click(function() {
+  next();
+})
+
+$('#previous').click(function() {
+  previous();
 })
